04f9c8e93c99d8f7a1af5acb7d9b8cbcbddb959c
Michuxx <104158118+Michuxx@users.noreply.github.com>
2022-12-17 22:14:13 +0100

    Front przygotowany do fetch'owania

--- src/Components/MainPage/MainPage.js
+++ src/Components/MainPage/MainPage.js
@@ -1,20 +1,34 @@
 import "../../css/MainPage.css";
 import React from "react";
-import Logo from "../../images/Logo.jpg";
-
+import Logo from "../../images/university.png";
+import Idea from "../../images/idea.png";
 
 function MainPage() {
-    return (
+  return (
     <div className="MainContainer">
-        <div className="NavBar">
-            <img src={Logo} alt="Logo" height={"100%"}/>
+      <div className="NavBar">
+        <img src={Logo} alt="Logo" height={"95%"} />
+        <p>Ni3 - I nie ma trójki</p>
+      </div>
+      <main>
+        <div className="Content">
+          <div className="wrapper2">
+            <div className="CourseLabel">
+              <img src={Idea} alt="icon" height={"96px"} />
+              <h1>Lista Kursów</h1>
+            </div>
+            <div className="Buttons">
+              <button type="button" className="Add">
+                +
+              </button>
+            </div>
+          </div>
+          <div className="Courses"></div>
         </div>
-        <main>
-            
-        </main>
-    </div>  
-    );
-  }
-  
-  export default MainPage;
-  
+      </main>
+      <footer>© 2022/2023 Ni3 | wszelkie prawa zastrzeżone :3</footer>
+    </div>
+  );
+}
+
+export default MainPage;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './css/index.css';
-import App from './Components/App';
-import reportWebVitals from './reportWebVitals';
-import AuthWrapper from './Components/LoginStart/AuthWrapper';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./css/index.css";
+import App from "./Components/App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-  
   </React.StrictMode>
 );
 
